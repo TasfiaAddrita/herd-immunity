fef714188162dbbcd04b121394e7081a86e95c56
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-06 12:33:46 -0700

    deleted docstrings and comments for cleaner space to work in, fixed misc bugs in starter code

--- virus.py
+++ virus.py
@@ -6,10 +6,10 @@ class Virus(object):
         self.repro_rate = repro_rate
         self.mortality_rate = mortality_rate
 
-
 def test_virus_instantiation():
     #TODO: Create your own test that models the virus you are working with
     '''Check to make sure that the virus instantiator is working.'''
+    
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
