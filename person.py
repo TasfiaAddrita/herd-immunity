644f668578dc11294b0d93542ffe5a5150d119f1
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-07 16:36:55 -0700

    completed person

--- person.py
+++ person.py
@@ -2,18 +2,23 @@ import random
 random.seed(42)
 from virus import Virus
 
-
 class Person(object):
     ''' Person objects will populate the simulation. '''
 
     def __init__(self, _id, is_vaccinated, infection=None):
-        self._id = None  # int
-        self.is_alive = True  # boolean
-        self.is_vaccinated = None  # boolean
-        self.infection = None  # Virus object or None
+        self._id = _id 
+        self.is_alive = True 
+        self.is_vaccinated = is_vaccinated 
+        self.infection = infection 
 
     def did_survive_infection(self):
-        pass
+        mortality = random.randint(0, 100)
+        if mortality < self.infection.mortality_rate * 100:
+            self.is_alive = False
+        else:
+            self.is_vaccinated = True
+        self.infection = None
+        return self.is_alive
 
 ''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
 def test_vacc_person_instantiation():
@@ -26,21 +31,54 @@ def test_vacc_person_instantiation():
 
 def test_not_vacc_person_instantiation():
     person = Person(2, False)
-    pass
+    # TODO: complete your own assert statements that test
+    # the values at each attribute
+    # assert ...
+    assert person._id == 2
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.infection is None
+
 
 def test_sick_person_instantiation():
+    # Create a Virus object to give a Person object an infection
     virus = Virus("Dysentery", 0.7, 0.2)
+    # Create a Person object and give them the virus infection
     person = Person(3, False, virus)
-    pass
+    # TODO: complete your own assert statements that test
+    # the values at each attribute
+    # assert ...
+    assert person._id == 3
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.infection is virus
+
 
 def test_did_survive_infection():
+    # TODO: Create a Virus object to give a Person object an infection
     virus = Virus("Dysentery", 0.7, 0.2)
+    # TODO: Create a Person object and give them the virus infection
     person = Person(4, False, virus)
 
+    # Resolve whether the Person survives the infection or not
     survived = person.did_survive_infection()
+    # Check if the Person survived or not
     if survived:
         assert person.is_alive is True
-        pass
+        # TODO: Write your own assert statements that test
+        # the values of each attribute for a Person who survived
+        # assert ...
+        assert person.is_vaccinated is True
+        assert person.infection is None
     else:
         assert person.is_alive is False
-        pass
+        # TODO: Write your own assert statements that test
+        # the values of each attribute for a Person who did not survive
+        # assert ...
+        assert person.infection is None
+
+if __name__ == "__main__":
+    test_vacc_person_instantiation()
+    test_not_vacc_person_instantiation()
+    test_sick_person_instantiation()
+    test_did_survive_infection()
--- simulation.py
+++ simulation.py
@@ -6,23 +6,63 @@ from virus import Virus
 
 class Simulation(object):
     def __init__(self, pop_size, vacc_percentage, virus, initial_infected=1):
-        self.logger = None
-        self.population = [] # List of Person objects
-        self.pop_size = pop_size # Int
-        self.next_person_id = 0 # Int
-        self.virus = virus # Virus object
-        self.initial_infected = initial_infected # Int
-        self.total_infected = 0 # Int
-        self.current_infected = 0 # Int
-        self.vacc_percentage = vacc_percentage # float between 0 and 1
-        self.total_dead = 0 # Int
-        self.file_name = "{}_simulation_pop_{}_vp_{}_infected_{}.txt".format(virus_name, pop_size, vacc_percentage, initial_infected)
+        self.population = [] 
+        self.pop_size = pop_size 
+        self.vacc_percentage = vacc_percentage 
+        self.virus = virus 
+        self.initial_infected = initial_infected 
+
+        self.next_person_id = 0 
+        self.total_infected = 0 
+        self.current_infected = 0 
+        self.total_dead = 0 
         self.newly_infected = []
 
-    def _create_population(self, initial_infected):
-        pass
+        self.file_name = "{}_simulation_pop_{}_vp_{}_infected_{}.txt".format(self.virus.name, pop_size, vacc_percentage, initial_infected)
+        self.logger = Logger(self.file_name)
+
+    def _create_population(self):
+        _id = 1
+        for _ in range(self.pop_size):
+            if _id <= self.initial_infected:
+                self.population.append(Person(_id, False, self.virus))
+            else:
+                self.population.append(Person(_id, True))
+            _id += 1
+        
+        for i in self.population:
+            print(i._id, i.is_vaccinated)
+
+        return self.population
 
     def _simulation_should_continue(self):
+        total_vaccinated = 0
+        for person in self.population:
+            if person.is_alive:
+                total_vaccinated += 1
+
+        if (self.total_dead == self.pop_size) or (total_vaccinated == self.pop_size):
+            return False
+        return True
+
+    def time_step(self):
+        pass
+
+    def interaction(self, person, random_person):
+        assert person.is_alive == True
+        assert random_person.is_alive == True
+
+        if random_person.is_vaccinated: 
+            print("Nothing happens") # replace with logger method
+        # elif random_person.infection != None:
+        #     print("Nothing happens pt 2")
+        else:
+            print("Something happens")
+            infected_chance = random.randint(0, 100) 
+            if infected_chance <= self.virus.repro_rate * 100:
+                random_person.infection = self.virus
+
+    def _infect_newly_infected(self):
         pass
 
     def run(self):
@@ -35,34 +75,24 @@ class Simulation(object):
         print('The simulation has ended after {time_step_counter} turns.'.format(time_step_counter))
         pass
 
-    def time_step(self):
-        pass
-
-    def interaction(self, person, random_person):
-        assert person.is_alive == True
-        assert random_person.is_alive == True
-
-        pass
-
-    def _infect_newly_infected(self):
-        pass
-
-
 if __name__ == "__main__":
-    params = sys.argv[1:]
-    virus_name = str(params[0])
-    repro_num = float(params[1])
-    mortality_rate = float(params[2])
+    # params = sys.argv[1:]
+    # virus_name = str(params[0])
+    # repro_num = float(params[1])
+    # mortality_rate = float(params[2])
 
-    pop_size = int(params[3])
-    vacc_percentage = float(params[4])
+    # pop_size = int(params[3])
+    # vacc_percentage = float(params[4])
 
-    if len(params) == 6:
-        initial_infected = int(params[5])
-    else:
-        initial_infected = 1
+    # if len(params) == 6:
+    #     initial_infected = int(params[5])
+    # else:
+    #     initial_infected = 1
 
-    virus = Virus(virus_name, repro_num, mortality_rate)
-    sim = Simulation(pop_size, vacc_percentage, initial_infected, virus)
+    # virus = Virus(virus_name, repro_num, mortality_rate)
+    # sim = Simulation(pop_size, vacc_percentage, initial_infected, virus)
 
-    sim.run()
+    virus = Virus('Ebola', 0.25, 0.70)
+    sim = Simulation(20, 0.90, virus, 10)
+    sim._create_population()
+    # sim.run()
